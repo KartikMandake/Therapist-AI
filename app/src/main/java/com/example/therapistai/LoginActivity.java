0af12a71450cabe2690abccb402fccd9d1ee2b35
saksham1326 <chaudharysaksham43@gmail.com>
2026-02-09 23:50:22 +0530

    push

--- app/src/main/java/com/example/therapistai/LoginActivity.java
+++ app/src/main/java/com/example/therapistai/LoginActivity.java
@@ -25,7 +25,7 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputEditText etEmail, etPassword;
     private MaterialButton btnLogin, btnRegister, btnGuestAccess;
     private ProgressBar progressBar;
-    
+    //commit
     // Firebase Authentication
     private FirebaseAuth firebaseAuth;
     //just a login push
